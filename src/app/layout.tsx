89a6f98142aaad315d448f20d29c6bfe6ae4fffa
Trocoli <ygoraraujoca@outlook.com>
2023-11-06 15:58:32 -0300

    delete file implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { cn } from './lib/utils';
-import NavBar from './components/NavBar';
-import Providers from './components/Providers';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "./lib/utils";
+import NavBar from "./components/NavBar";
+import Providers from "./components/Providers";
 
-const inter = Inter({ subsets: ['latin'] })
+import "react-loading-skeleton/dist/skeleton.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DocZap.',
-  description: 'Bata um papo com seus PDF\'s',
-}
+  title: "DocZap.",
+  description: "Bata um papo com seus PDF's",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className='light'>
+    <html lang="en" className="light">
       <Providers>
-
-      <body className={cn(
-        'min-h-screen font-sans antialiased grainy', inter.className 
-      )}>
-        <NavBar />
-        {children}</body>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased grainy",
+            inter.className
+          )}
+        >
+          <NavBar />
+          {children}
+        </body>
       </Providers>
     </html>
-  )
+  );
 }
