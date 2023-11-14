966ec0671c4ab1ab6f48eea45000cd2646ffea76
Trocoli <ygoraraujoca@outlook.com>
2023-11-14 12:12:13 -0300

    messages prisma schema

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,16 +1,25 @@
 import { SendMessageValidator } from "@/app/lib/validators/SendMessageValidator";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
- // endpoint to ask question to pdf file
+  // endpoint to ask question to pdf file
 
- const body = await req.json()
- const {getUser} = getKindeServerSession()
- const user = getUser()
+  const body = await req.json();
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
- const {id: userId} = user
- if (!userId) return new Response('Unauthorized', {status: 401})
+  const { id: userId } = user;
+  if (!userId) return new Response("Unauthorized", { status: 401 });
 
- const {} = SendMessageValidator.parse(body)
-} 
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({ where: { id: fileId, userId } });
+
+  if (!file) {
+    return new Response("Not Found", { status: 404 });
+  }
+
+  await db.message.create()
+};
